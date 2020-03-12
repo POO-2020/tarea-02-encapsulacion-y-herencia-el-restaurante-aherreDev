bbe711a8aa3370a99186508df59be15fea2e3ef1
AherreDev <aherredev@gmail.com>
2020-03-11 18:54:33 -0600

    [Add] index file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // ? Models
 
 import Cliente from "./models/Cliente.model.js";
+import ClienteFrecuente from "./models/ClienteFrecuente.model.js";
 import Direccion from "./models/Direccion.model.js";
 import ElementoPedido from "./models/ElementoPedido.model.js";
 import Pedido from "./models/Pedido.model.js";
@@ -16,9 +17,9 @@ import Tiempo from "./models/Tiempo.model.js";
 
 // * Cliente
 
-let cliente = new Cliente(
-  "Roman Herrera",
-  new Direccion(
+let cliente = new Cliente({
+  nombre: "Roman Herrera",
+  direccion: new Direccion(
     "Alfonso Reyes",
     434,
     3,
@@ -27,8 +28,8 @@ let cliente = new Cliente(
     "Colima",
     "Colima"
   ),
-  "312312312"
-);
+  telefono: "312312312"
+});
 console.log(cliente.getPerfil());
 
 // * Producto
@@ -45,12 +46,13 @@ console.log(elementoPedido.getDescripcion());
 
 // * Pedido
 
-let pedido = new Pedido(
-  new Fecha(2020, 1, 10),
-  new Tiempo(10, 20, "AM"),
-  cliente,
-  [elementoPedido]
-);
+let pedido = new Pedido({
+  fecha: new Fecha(2020, 1, 10),
+  hora: new Tiempo(10, 20, "AM"),
+  cliente: cliente,
+  elementosPedidos: [elementoPedido],
+  numeroPedido: 10
+});
 
 console.log(pedido.getResumen());
 
@@ -70,16 +72,18 @@ pedido.listarElemento();
 
 // * Restaurante
 
-let restaurante = new Restaurante(
-  "Roman's Restaurant",
-  "312123",
-  "Alfonsadasd asd",
-  [producto, producto2],
-  [pedido]
-);
+let restaurante = new Restaurante({
+  nombre: "Roman's Restaurant",
+  telefono: "312123",
+  direccion: "Alfonsadasd asd",
+  productos: [producto, producto2],
+  pedidos: [pedido]
+});
 
 restaurante.listarPedidos();
 restaurante.listarProductos();
+
+console.log("------------");
 
 restaurante.registrarPedido(pedido);
 restaurante.listarPedidos();
@@ -87,3 +91,33 @@ restaurante.listarPedidos();
 let producto3 = new Producto(new Precio(15), "Salsa");
 restaurante.registrarProducto(producto3);
 restaurante.listarProductos();
+
+let CF = new ClienteFrecuente({
+  nombre: "Roman Herrera",
+  direccion: new Direccion(
+    "Esta es una direccion",
+    434,
+    3,
+    "Colonia Ajua",
+    "111",
+    "Colima",
+    "Colima"
+  ),
+  telefono: "312312312",
+  numeroCliente: 123,
+  fechaRegistro: new Fecha(2020, 1, 10)
+});
+console.log(CF.getPerfil());
+
+// * Test CRUD methods of Restaurant
+let pedido2 = new Pedido({
+  fecha: new Fecha(2020, 1, 10),
+  hora: new Tiempo(10, 20, "AM"),
+  cliente: cliente,
+  elementosPedidos: [elementoPedido],
+  numeroPedido: 11
+});
+
+console.log(restaurante.registrarPedido(pedido2));
+console.log(restaurante.buscarPedido(pedido2));
+console.log(restaurante.eliminarPedido(pedido2));
