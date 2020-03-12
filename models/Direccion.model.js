98402ff1f346b951bab621fd9ee0b02cd431e17a
AherreDev <aherredev@gmail.com>
2020-03-11 18:13:42 -0600

    [Update] Direccion model

--- models/Direccion.model.js
+++ models/Direccion.model.js
@@ -1,11 +1,11 @@
 class Direccion {
-  calle;
-  numInt;
-  numExt;
-  col;
-  cp;
-  ciudad;
-  municipio;
+  _calle;
+  _numInt;
+  _numExt;
+  _col;
+  _cp;
+  _ciudad;
+  _municipio;
   /**
    * @param {string} calle
    * @param {number} numInt
@@ -15,18 +15,18 @@ class Direccion {
    * @param {string} ciudad
    * @param {string} municipio
    */
-  constructor(calle, numInt = null, numExt, col, cp, ciudad, municipio) {
-    this.calle = calle;
-    this.numInt = numInt;
-    this.numExt = numExt;
-    this.col = col;
-    this.cp = cp;
-    this.ciudad = ciudad;
-    this.municipio = municipio;
+  constructor({ calle, numInt = null, numExt, col, cp, ciudad, municipio }) {
+    this._calle = calle;
+    this._numInt = numInt;
+    this._numExt = numExt;
+    this._col = col;
+    this._cp = cp;
+    this._ciudad = ciudad;
+    this._municipio = municipio;
   }
-  getFormatoCorto = () => `${this.calle} ${this.numExt}`;
+  getFormatoCorto = () => `${this._calle} ${this._numExt}`;
   getFormatoLargo = () =>
-    `${this.calle} ${this.numInt} ${this.numExt} ${this.col} ${this.cp} ${this.ciudad} ${this.municipio}`;
+    `${this._calle} ${this._numInt} ${this._numExt} ${this._col} ${this._cp} ${this._ciudad} ${this._municipio}`;
 }
 
 export default Direccion;
